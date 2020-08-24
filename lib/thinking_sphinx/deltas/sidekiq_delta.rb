17b2305ef1471ec8aa9f1a7e41a9928829b19bdc
bikeath1337 <bikeath1337@me.com>
2020-08-24 17:53:01 +0200

    puts

--- lib/thinking_sphinx/deltas/sidekiq_delta.rb
+++ lib/thinking_sphinx/deltas/sidekiq_delta.rb
@@ -2,12 +2,14 @@ require 'thinking_sphinx'
 
 class ThinkingSphinx::Deltas::SidekiqDelta < ThinkingSphinx::Deltas::DefaultDelta
   def delete(index, instance)
+    puts "Deleting #{index} in background"
     ThinkingSphinx::Deltas::SidekiqDelta::FlagAsDeletedJob.perform_later(
       index.name, index.document_id_for_key(instance.id)
     )
   end
 
   def index(index)
+    puts "INDEXING #{index} in background"
     ThinkingSphinx::Deltas::SidekiqDelta::DeltaJob.perform_later(index.name)
   end
 end
